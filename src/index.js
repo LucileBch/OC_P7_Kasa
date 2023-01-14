c0728a352f8783420dae73f4e92b59e6f37492a9
LucileBch <l.bouchire@gmail.com>
2023-01-14 15:47:27 +0100

    Creating routing V1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Logement from './pages/Logement';
+import About from './pages/About';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Home />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/logement" element={<Logement />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
