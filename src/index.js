03e6e2a70b1ebcb0e58f98fe63f2689d11bcce9c
LucileBch <l.bouchire@gmail.com>
2023-01-14 16:35:58 +0100

    Routing and link

--- src/index.js
+++ src/index.js
@@ -4,15 +4,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Logement from './pages/Logement';
 import About from './pages/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Error from './components/Error';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
+            <Header />
+            LOGO KASA
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/logement" element={<Logement />} />
                 <Route path="/about" element={<About />} />
+                <Route path="*" element={<Error />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
