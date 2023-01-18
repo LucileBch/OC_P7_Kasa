fe979efeeec55d0eab5fa036b420384de72f3448
LucileBch <l.bouchire@gmail.com>
2023-01-18 14:55:40 +0100

    Separating router

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Logement from './pages/Logement';
-import About from './pages/About';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Error from './components/Error';
+import { BrowserRouter } from 'react-router-dom';
+import './styles/index.css';
+import App from './components/App/index.jsx';
 
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <Header />
-            LOGO KASA
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/logement" element={<Logement />} />
-                <Route path="/about" element={<About />} />
-                <Route path="*" element={<Error />} />
-            </Routes>
-            <Footer />
+            <App />
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
