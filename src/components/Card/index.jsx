02202a572f0f7d6f0eff919bddeb2fb910df6cf1
LucileBch <l.bouchire@gmail.com>
2023-01-25 19:53:41 +0100

    Adding NavLink in Card component

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import { appartList } from '../../datas/appartList';
 import '../../styles/Card.css';
 
@@ -5,14 +6,16 @@ function Card({ id, title, cover }) {
     return (
         <section className="appart-list">
             {appartList.map(({ id, cover, title }) => (
-                <article key={id} className="appart-card">
-                    <img
-                        src={cover}
-                        alt={`${title} cover`}
-                        className="card-img"
-                    ></img>
-                    <h3 className="card-title">{title}</h3>
-                </article>
+                <NavLink to={`/logement/${id}`} key={id}>
+                    <article key={id} className="appart-card">
+                        <img
+                            src={cover}
+                            alt={`${title} cover`}
+                            className="card-img"
+                        ></img>
+                        <h3 className="card-title">{title}</h3>
+                    </article>
+                </NavLink>
             ))}
         </section>
     );
