9734dfd86f7fed49d26cb094466e755b65d5ed2f
LucileBch <l.bouchire@gmail.com>
2023-01-25 18:38:25 +0100

    Correcting Collapse component

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,33 +1,26 @@
 import { useState } from 'react';
-import { aboutContent } from '../../datas/aboutContent';
 import arrowDown from '../../assets/arrow-down.png';
 import arrowUp from '../../assets/arrow-up.png';
+import '../../styles/Collapse.css';
 
-function Collapse({ id, title, description }) {
+function Collapse(props) {
     const [isOpen, setIsOpen] = useState(false);
-    return isOpen ? (
-        <section>
-            {aboutContent.map(({ id, title, description }) => (
-                <article key={id}>
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                    <button onClick={() => setIsOpen(false)}>
-                        <img src={arrowUp} alt="flèche de fermeture" />
-                    </button>
-                </article>
-            ))}
-        </section>
-    ) : (
-        <section>
-            {aboutContent.map(({ id, title }) => (
-                <article key={id}>
-                    <h2>{title}</h2>
-                    <button onClick={() => setIsOpen(true)}>
-                        <img src={arrowDown} alt="flèche d'ouverture" />
-                    </button>
-                </article>
-            ))}
-        </section>
+    const toogle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <article className="collapse">
+            <div className="collapse-open" onClick={toogle}>
+                <h2 className="collapse-title">{props.title}</h2>
+                {isOpen ? (
+                    <img src={arrowUp} alt="closing arrow" />
+                ) : (
+                    <img src={arrowDown} alt="opening arrow" />
+                )}
+            </div>
+            {isOpen && <p>{props.content}</p>}
+        </article>
     );
 }
 
