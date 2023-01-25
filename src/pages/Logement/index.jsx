e72cd74330426e1da46c17e18c4070e4977ba083
LucileBch <l.bouchire@gmail.com>
2023-01-25 22:13:24 +0100

    Adding error route if id not found, header and footer

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,10 +1,18 @@
 import { useParams } from 'react-router-dom';
+import { appartList } from '../../datas/appartList';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import Error from '../../pages/Error';
 
 function Logement() {
     const { id } = useParams();
+    const appartContent = appartList.find((appart) => appart.id === id);
+    if (!appartContent) return <Error />;
     return (
         <div>
+            <Header />
             <h1>Fiche logement {id} </h1>
+            <Footer />
         </div>
     );
 }
