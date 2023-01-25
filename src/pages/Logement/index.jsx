fc4284ecc4a17cc4b56f54c540f3b8a320271331
LucileBch <l.bouchire@gmail.com>
2023-01-25 23:43:41 +0100

    Adding Tag component on Logement page

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,16 +1,24 @@
 import { useParams } from 'react-router-dom';
 import { appartList } from '../../datas/appartList';
 import Header from '../../components/Header';
+import Tag from '../../components/Tag';
 import Footer from '../../components/Footer';
 import Error from '../../pages/Error';
 
 function Logement() {
     const { id } = useParams();
-    const appartContent = appartList.find((appart) => appart.id === id);
-    if (!appartContent) return <Error />;
+    const appartDisplay = appartList.filter((appart) => appart.id === id);
+
+    if (!appartDisplay) return <Error />;
+
     return (
         <div>
             <Header />
+            <section>
+                {appartDisplay.map(({ id, tags }) => (
+                    <Tag key={id} tags={tags} />
+                ))}
+            </section>
             <h1>Fiche logement {id} </h1>
             <Footer />
         </div>
