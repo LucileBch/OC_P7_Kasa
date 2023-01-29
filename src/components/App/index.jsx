2597822384b137f030c48d1ef38817a002bcfb44
LucileBch <l.bouchire@gmail.com>
2023-01-29 17:46:42 +0100

    Comments

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,13 @@
+/*     ROUTING LOGIC     */
+
+/* Importing React routes and the different pages */
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../../pages/Home';
 import Logement from '../../pages/Logement';
 import About from '../../pages/About';
 import Error from '../../pages/Error';
 
+/* Routing function */
 function App() {
     return (
         <BrowserRouter>
@@ -17,4 +21,5 @@ function App() {
     );
 }
 
+/* Exporting routing function */
 export default App;
