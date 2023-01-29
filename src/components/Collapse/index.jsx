b50978c92ff37366edba59d8760e494b8f09d0a8
LucileBch <l.bouchire@gmail.com>
2023-01-29 23:19:01 +0100

    Comments

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -1,27 +1,35 @@
+/***     COLLAPSE COMPONENT     ***/
+
+/* Importing useState Hook and assets */
 import { useState } from 'react';
 import arrowDown from '../../assets/arrow-down.png';
 import arrowUp from '../../assets/arrow-up.png';
 import '../../styles/Collapse.css';
 
-function Collapse(props) {
+/* Collapse function  using props title and content */
+/* Hook useState to manage isOpen state */
+/* Handler onClick to open or close the component */
+/* Ternary operator to indicate which infos to display according to the state */
+function Collapse({ title, content }) {
     const [isOpen, setIsOpen] = useState(false);
-    const toogle = () => {
+    const button = () => {
         setIsOpen(!isOpen);
     };
 
     return (
         <article className="collapse">
-            <div className="collapse-open" onClick={toogle}>
-                <h2 className="collapse-title">{props.title}</h2>
+            <div className="collapse-open" onClick={button}>
+                <h2 className="collapse-title">{title}</h2>
                 {isOpen ? (
                     <img src={arrowUp} alt="closing arrow" />
                 ) : (
                     <img src={arrowDown} alt="opening arrow" />
                 )}
             </div>
-            {isOpen && <div>{props.content}</div>}
+            {isOpen && <div>{content}</div>}
         </article>
     );
 }
 
+/* Exporting Collapse function */
 export default Collapse;
