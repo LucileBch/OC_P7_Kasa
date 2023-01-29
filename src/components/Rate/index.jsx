1fb10693d0b2f5cf0a563e0252f69575cc73f007
LucileBch <l.bouchire@gmail.com>
2023-01-30 00:25:55 +0100

    Comments

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -1,5 +1,11 @@
+/***     RATE COMPONENT     ***/
+
+/* Importing assets */
 import fullStar from '../../assets/full-star.png';
 import emptyStar from '../../assets/empty-star.png';
+
+/* Rate function using prop stars */
+/* Using range.map to compare each number with the number of stars and display full and/or empty stars*/
 
 function Rate({ stars }) {
     const range = [1, 2, 3, 4, 5];
@@ -17,4 +23,5 @@ function Rate({ stars }) {
     );
 }
 
+/* Exporting Rate component */
 export default Rate;
