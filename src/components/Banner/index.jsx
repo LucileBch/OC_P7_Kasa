f8b85c8765bb641d15abe5328bbf9b690215e55e
LucileBch <l.bouchire@gmail.com>
2023-02-03 00:55:21 +0100

    Comments

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,7 +3,7 @@
 /* Importing Banner style */
 import '../../styles/Banner.css';
 
-/* Function banner using props imagebanner and title */
+/* Function banner using props imagebanner, title and page */
 function Banner({ imagebanner, title, page }) {
     const bannerPicture =
         page === 'about' ? 'banner-picture_about' : 'banner-picture_home';
