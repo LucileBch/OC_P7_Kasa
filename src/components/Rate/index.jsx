9a460b9f82a5dc6c1d2b8d7ad403a75c5028946b
LucileBch <l.bouchire@gmail.com>
2023-02-02 22:02:49 +0100

    Adding Rate style

--- src/components/Rate/index.jsx
+++ src/components/Rate/index.jsx
@@ -3,6 +3,7 @@
 /* Importing assets */
 import fullStar from '../../assets/full-star.png';
 import emptyStar from '../../assets/empty-star.png';
+import '../../styles/Rate.css';
 
 /* Rate function using prop stars */
 /* Using range.map to compare each number with the number of stars and display full and/or empty stars*/
@@ -10,7 +11,7 @@ function Rate({ stars }) {
     const range = [1, 2, 3, 4, 5];
 
     return (
-        <div>
+        <div className="stars">
             {range.map((rangeElem, star) =>
                 stars >= rangeElem ? (
                     <img src={fullStar} alt="pink star" key={star} />
